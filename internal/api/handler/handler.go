ec5c1870fc997ceb48ef334ee56b1657a00a0103
DanielTitkov <mytasmic@gmail.com>
2021-02-23 21:22:38 +0300

    handler update

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/DanielTitkov/correlateme-server/internal/configs"
 	"github.com/DanielTitkov/correlateme-server/internal/logger"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type Handler struct {
@@ -29,7 +30,12 @@ func NewHandler(
 }
 
 func (h *Handler) link(e *echo.Echo) {
+
 	v1 := e.Group("/api/v1")
-	v1.POST("/listAnomalies", h.ListAnomaliesHandler)
-	v1.POST("/setAnomalyStatus", h.SetAnomalyStatusHandler)
+	v1.POST("/getToken", h.GetTokenHandler)
+	v1.POST("/createUser", h.CreateUserHandler)
+	// Restricted group
+	v1Restricted := e.Group("/private")
+	v1Restricted.Use(middleware.JWT([]byte(h.cfg.Auth.Secret)))
+	v1Restricted.POST("/getUser", h.GetUserHandler)
 }
