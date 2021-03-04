e4774f95feeabdd9a5353b41e5ebb6e70fe2e185
DanielTitkov <mytasmic@gmail.com>
2021-03-04 23:30:55 +0300

    draft

--- cmd/app/prepare/server.go
+++ cmd/app/prepare/server.go
@@ -5,13 +5,17 @@ import (
 	"github.com/DanielTitkov/correlateme-server/internal/app"
 	"github.com/DanielTitkov/correlateme-server/internal/configs"
 	"github.com/DanielTitkov/correlateme-server/internal/logger"
+	echoPrometheus "github.com/globocom/echo-prometheus"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func NewServer(cfg configs.Config, logger *logger.Logger, app *app.App) *echo.Echo {
 	e := echo.New()
-
+	// Enable metrics middleware
+	e.Use(echoPrometheus.MetricsMiddleware())
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
--- go.mod
+++ go.mod
@@ -7,10 +7,12 @@ require (
 	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dgryski/go-onlinestats v0.0.0-20170612111826-1c7d19468768
+	github.com/globocom/echo-prometheus v0.1.2
 	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/echo-contrib v0.9.0
 	github.com/lib/pq v1.9.0
 	github.com/mxschmitt/golang-combinations v1.1.0
+	github.com/prometheus/client_golang v1.9.0
 	github.com/robfig/cron v1.2.0
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	go.uber.org/zap v1.16.0
